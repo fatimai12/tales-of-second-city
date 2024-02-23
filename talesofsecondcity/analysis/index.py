ee292d2fe3eb599ae11d458d59f3ee706ecd57ce
Victoria Marie Beck <beckv@linux2.cs.uchicago.edu>
2024-02-23 10:40:59 -0600

    initial commit of index file

---
+++ talesofsecondcity/analysis/index.py
@@ -0,0 +1,19 @@
+# Create an index for access to public services using parks, library, and  
+# transit data.
+
+import pandas as pd
+
+### Read in data as pandas data frames ###
+
+# Parks data
+
+
+# Library data
+
+
+# Transit data
+
+
+# HUD Zipcode crosswalk 
+
+### 

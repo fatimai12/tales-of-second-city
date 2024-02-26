27b860a439e782edd7be8a41c250acdb8ce45026
fatimai12 <142926806+fatimai12@users.noreply.github.com>
2024-02-25 19:45:46 -0600

    more viz

--- talesofsecondcity/visualization/map.py
+++ talesofsecondcity/visualization/map.py
@@ -6,12 +6,40 @@ from pygris import tracts, counties
 from pygris.utils import shift_geometry
 from pygris.data import get_census
 
+#fatima's work
 # shapefile_path = "../data/original/tl_2022_17_tract.shp"
-shapefile_path = "talesofsecondcity/data/original/Boundaries - Census Tracts - 2010.geojson"
-gdf = gpd.read_file(shapefile_path)
-gdf.plot()
-gdf.plot()
+census_shapefile_path = "talesofsecondcity/data/original/Boundaries - Census Tracts - 2010.geojson"
+city_shapefile_path = "talesofsecondcity/data/original/Boundaries - City.geojson"
 
+census_map = gpd.read_file(census_shapefile_path)
+city_map = gpd.read_file(city_shapefile_path)
+fig, ax = plt.subplots(ncols = 2)
+census_map.plot(ax = ax[0])
+city_map.plot(ax = ax[1])
+ax[0].set_title("Census tracts")
+ax[1].set_title("City boundary")
+plt.show()
+
+tracts_cartographic_21 = tracts(state = "IL", county = "Cook", cb = True, cache = True, year = 2021)
+tracts_cartographic_20 = tracts(state = "IL", county = "Cook", cb = True, cache = True, year = 2020)
+fig, ax = plt.subplots(ncols = 2)
+tracts_cartographic_21.plot(ax = ax[0])
+tracts_cartographic_20.plot(ax = ax[1])
+ax[0].set_title("Tracts 2021")
+ax[1].set_title("Tracts 2020")
+plt.show()
+
+
+# Plot the city boundaries
+ax = city_map.plot(color='none', edgecolor='black')
+
+# Plot the census tracts on top of the city boundaries
+census_map.plot(ax=ax, color='red', alpha=0.5)
+
+# Show the plot
+plt.show()
+
+#https://walker-data.com/pygris/03-data-utilities/
 
 # Census tracts for 2022 data 
 acs5_2022 = pd.read_csv('../data/original/acs5_data_2022.csv')
@@ -27,45 +55,3 @@ chi_tracts = il_tracts[il_tracts['COUNTYFP'] == '031']
 
 #this merge doesnt actually work that well 
 chi_map = (chi_tracts.merge(acs5_2022, how = 'left', left_on = 'GEOID', right_on = 'GEOID'))
-
-
-
-chicago_tracts_cartographic_21 = tracts(state = "IL", county = "Cook", cb = True, cache = True, year = 2021)
-chicago_tracts_cartographic_20 = tracts(state = "IL", county = "Cook", cb = True, cache = True, year = 2020)
-
-
-# Plot the two side-by-side to compare them
-fig, ax = plt.subplots(ncols = 2)
-
-chicago_tracts_cartographic_21.plot(ax = ax[0])
-chicago_tracts_cartographic_20.plot(ax = ax[1])
-
-
-ax[0].set_title("TIGER/Line")
-ax[1].set_title("Cartographic")
-
-https://walker-data.com/pygris/03-data-utilities/
-
-us_youth_sahie = get_census(dataset = "timeseries/healthins/sahie",
-                            variables = "PCTUI_PT",
-                            params = {
-                                "for": "county:*",
-                                "in": "state:*",
-                                "time": 2019,
-                                "AGECAT": 4
-                            }, 
-                            return_geoid = True, 
-                            guess_dtypes = True)
-
-us_counties = counties(cb = True, resolution = "20m", cache = True, year = 2019)
-us_counties_rescaled = shift_geometry(us_counties)
-
-us_counties_merged = us_counties_rescaled.merge(us_youth_sahie, on = "GEOID")
-
-us_counties_merged.plot(
-    column = "PCTUI_PT",
-    cmap = "viridis",
-    figsize = (8, 6)
-)
-
-plt.title("% uninsured under age 19 by county, 2019")

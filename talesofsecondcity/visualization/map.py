61f6ed5d8f7922398070fdd48caf1af70a9bce99
fatimai12 <142926806+fatimai12@users.noreply.github.com>
2024-02-25 18:05:54 -0600

    app.py, viz

--- talesofsecondcity/visualization/map.py
+++ talesofsecondcity/visualization/map.py
@@ -2,7 +2,8 @@ import plotly.express as px
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-shapefile_path = "../data/original/tl_2022_17_tract.shp"
+# shapefile_path = "../data/original/tl_2022_17_tract.shp"
+shapefile_path = "talesofsecondcity/data/original/Boundaries - Census Tracts - 2010.geojson"
 gdf = gpd.read_file(shapefile_path)
 gdf.plot()
-gdf.plot()
+plt.show()

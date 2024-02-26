8a7696960759eb19314beee678840d87cfc140ec
SuchiTailor <suchitailor@gmail.com>
2024-02-25 18:47:30 -0600

    data + map updates

--- talesofsecondcity/visualization/map.py
+++ talesofsecondcity/visualization/map.py
@@ -1,8 +1,25 @@
 import plotly.express as px
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 shapefile_path = "../data/original/tl_2022_17_tract.shp"
 gdf = gpd.read_file(shapefile_path)
 gdf.plot()
 gdf.plot()
+
+
+# Census tracts for 2022 data 
+acs5_2022 = pd.read_csv('../data/original/acs5_data_2022.csv')
+
+#convert codes to strings to create GEOID
+acs5_2022["State code"] = acs5_2022["State code"].astype(str)
+acs5_2022["County code"] = acs5_2022["County code"].astype(str)
+acs5_2022["Tract Code"] = acs5_2022["Tract Code"].astype(str)
+acs5_2022["GEOID"] = acs5_2022["State code"] + acs5_2022["County code"] + acs5_2022["Tract Code"]
+
+il_tracts = gpd.read_file('cb_2022_17_tract_500k.zip')
+chi_tracts = il_tracts[il_tracts['COUNTYFP'] == '031']
+
+#this merge doesnt actually work that well 
+chi_map = (chi_tracts.merge(acs5_2022, how = 'left', left_on = 'GEOID', right_on = 'GEOID'))

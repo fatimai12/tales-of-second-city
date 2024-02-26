87d8cc2b85132192208b4d262b12f729b0b27405
Victoria Marie Beck <beckv@linux2.cs.uchicago.edu>
2024-02-26 13:34:53 -0600

    finished index, just need to load data into it

--- talesofsecondcity/analysis/index.py
+++ talesofsecondcity/analysis/index.py
@@ -15,7 +15,6 @@ parks_acreage = pd.read_csv("", usecols = ["PARK_NO", "ACRES"])
 parks_acreage = parks_acreage.set_index("PARK_NO")
 parks = parks.join(parks_acreage)
 
-
 libraries = pd.read_csv("")
 bus_stops = pd.read_csv("")
 L_stops = pd.read_csv("")
@@ -25,12 +24,6 @@ census_data = pd.read_csv("", usecols = ["TRACT", "TOTAL POP"])
 
 dataframes = {"Parks": parks, "Libraries": libraries, "Bus": bus_stops,
               "L": L_stops, "Divvy": divvy}
-
-THRESHOLDS = {"Parks": "low = , medium = , high =",
-              "Libraries": "??",
-              "Bus": "??",
-              "L": "??",
-              "Divvy": "??"}
 
 def link_data(services_data, pop_data):
     """
@@ -45,7 +38,8 @@ def link_data(services_data, pop_data):
             df = df.groupby("Tract").count().reset_index(name = "Count")
         df = df.merge(pop_data, left_on = "Tract", right_on = "Tract")
 
-def calculate_scores(df, service_col):
+
+def calculate_scores(df, df_name, service_col):
     """
     Calculate score for each tract in a data set based on proportion of services  
     to total population, and define thresholds
@@ -54,10 +48,31 @@ def calculate_scores(df, service_col):
     """
     # divide count by total population
     df["proportion"] = df[service_col] / df["TOTAL POP"]
-    # define thresholds for each category (look this up)
-    # for index, row in df.iterrows():
-    #    if row.proportion <
-    # score census tract by thresholds
+
+    # find distribution of proportion and split into 5 groups 
+    thresholds = pd.cut(df["proportion"], 5)
+    level_1 = thresholds[0]
+    level_2 = thresholds[1]
+    level_3 = thresholds[2]
+    level_4 = thresholds[3]
+    level_5 = thresholds[4]
+
+    col_name = df_name + "Score"
+    df[col_name] = 0
+    
+    for index, row in df.iterrows():
+        if row.proportion in level_1:
+            df.at[index, col_name] = 1
+        elif row.proportion in level_2:
+            df.at[index, col_name] = 2
+        elif row.proportion in level_3:
+            df.at[index, col_name] = 3
+        elif row.proportion in level_4:
+            df.at[index, col_name] = 4
+        elif row.proportion in level_5:
+            df.at[index, col_name] = 5
+
+    df = df.drop(columns = ["proportion"])
 
 
 def calculate_index(data: dict):
@@ -71,10 +86,10 @@ def calculate_index(data: dict):
     # Calculate scores for each public services data frame
     for key, df in data.items():
         if key == "Parks":
-            calculate_scores(df, "Total Acreage")
+            calculate_scores(df, key, "Total Acreage")
             df = df.reset_index()
         else:
-            calculate_scores(df, "Count")
+            calculate_scores(df, key, "Count")
         df = df.set_index("Tract")
         df = df[["Tract", "Score"]]
 
@@ -82,7 +97,8 @@ def calculate_index(data: dict):
     # service scores
     full_index_df =  pd.concat([data["Parks"], data["Libraries"], data["Bus"],
                                 data["L"], data["Divvy"]])
-    full_index_df.columns = ["Tract", "Parks Score"]
+    #####
+
 
     full_index_df["Transit Score"] = full_index_df["Bus Score"] + \
         full_index_df["L Score"] + full_index_df["Divvy Score"]
@@ -93,6 +109,3 @@ def calculate_index(data: dict):
 
     # normalize 0-1
     full_index_df["APS Index"] = full_index_df["Total Score"] / 45
-
-    
-
